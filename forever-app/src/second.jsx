05a39aba0e7b48bea241308b37c3700943b83cd7
Srikar Parsi <srikar.parsi@expensify.com>
2022-10-01 19:49:20 -0400

    dropdown and video

--- forever-app/src/second.jsx
+++ forever-app/src/second.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 //Functional Component 
+
+const options = [
+    'one', 'two', 'three'
+  ];
+  const defaultOption = options[0];
+
 const Second = () => {
   return (
     <div>
@@ -8,6 +16,7 @@ const Second = () => {
             Second Screen
         </h1>
         <Link to="/third">Show List of Users</Link>
+        <Dropdown options={options} onChange={console.log("pressed")} value={defaultOption} placeholder="Select an option" />
     </div>
     
   );
--- forever-app/src/third.jsx
+++ forever-app/src/third.jsx
@@ -2,9 +2,7 @@ import React from "react";
 //Functional Component 
 const Third = () => {
   return (
-    <h1>
-        Third Screen
-    </h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/NrM715M8vpU?start=10" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   );
 };
 
